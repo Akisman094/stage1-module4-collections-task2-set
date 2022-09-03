7c2b233b19de1a51276ba55e8a785256955f1e19
Akmal Ismoilov <master80098@gmail.com>
2022-09-03 23:24:16 +0500

    Task completed!

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,28 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (Integer next : sourceList)
+        {
+            if (next % 2 == 0)
+                evenNumbers(next, result);
+            else
+                oddNumbers(next, result);
+        }
+        return result;
+    }
+
+    public void evenNumbers(Integer num, HashSet<Integer> sourceList) {
+        if(!sourceList.contains(num))
+            sourceList.add(num);
+        if(num % 2 == 0)
+            evenNumbers(num / 2, sourceList);
+    }
+
+    public void oddNumbers(Integer num, HashSet<Integer> sourceList) {
+        if(!sourceList.contains(num))
+            sourceList.add(num);
+        if(!sourceList.contains(num * 2))
+            sourceList.add(num * 2);
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,10 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        return sourceList.stream().map(x -> x * x).filter(x -> x >= lowerBound && x <= upperBound).collect(Collectors.toSet());
     }
 }
